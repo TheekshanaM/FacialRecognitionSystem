aaefc3e6ce997d2437bff21b611882b1ec1d9f98
TheekshanaM <93.madushan@gmail.com>
2018-01-26 11:45:00 +0530

    add celebrity photo

--- FacialRecognitionSystem/Controllers/CelebrityController.cs
+++ FacialRecognitionSystem/Controllers/CelebrityController.cs
@@ -205,6 +205,94 @@ namespace FacialRecognitionSystem.Controllers
                 db.SaveChanges();
                 return RedirectToAction("Changesetting", "Celebrity",new { celebrity.CelebrityId});
             }
-        } 
+        }
+
+        public ActionResult ViewImage(int id)
+        {
+            using(MyDbEntities db = new MyDbEntities())
+            {
+                var imageList = db.CelebrityPhotoes.Where(a => a.CelibrityID == id).ToList();
+                if(imageList != null)
+                {
+                    ViewBag.Message = id;
+                    return View(imageList);
+                }
+                return RedirectToAction("Changesetting", "Celebrity", new { id});
+            }
+        }
+        
+        public async Task<ActionResult> AddPhoto(CreateCelebrityViewModel model)
+        {
+            string message = "";
+            HttpPostedFileBase file = model.imageBrowes;
+
+            DateTime dTime = DateTime.Now;
+            string time = dTime.ToString();
+            time = time.Replace(" ", "_") + ".jpg";
+
+            if (file.ContentLength > 0)
+            {
+
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=faceitphotos;AccountKey=67nq3VNJlZ0KJArJZU62vjri4pNzqd1MERWFQytw7w7B6cfTv7Gw75iJq4LJgUN7E05Y0+3ixmkOWDyKk4yhtw==");
+                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+                CloudBlobContainer container = blobClient.GetContainerReference("celebrityimages");
+                container.CreateIfNotExists();
+
+                CloudBlockBlob blockBlob = container.GetBlockBlobReference(model.CelebrityId.ToString() + "_" + time);
+                Image i = Image.FromStream(file.InputStream, true, true);
+                MemoryStream ms = new MemoryStream();
+                i.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                byte[] imgData = ms.ToArray();
+
+                using (var fileStream = new MemoryStream(imgData))
+                {
+                    await blockBlob.UploadFromStreamAsync(fileStream);
+                }
+
+                using(MyDbEntities db = new MyDbEntities())
+                {
+                    var photos = db.CelebrityPhotoes.Where(a => a.CelibrityID == model.CelebrityId).ToList();
+                    foreach(var photo in photos)
+                    {
+                        photo.ProfilePic = false;
+                        db.SaveChanges();
+                    }
+                }
+                //
+                CelebrityPhoto photoModel = new CelebrityPhoto();
+                photoModel.Link = "https://faceitphotos.blob.core.windows.net/celebrityimages/" + model.CelebrityId + "_" + time;
+                photoModel.ProfilePic = true;
+                photoModel.CelibrityID = model.CelebrityId;
+                var serializer = new JavaScriptSerializer();
+                var json2 = serializer.Serialize(photoModel);
+                var stringContent2 = new StringContent(json2, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync("api/Celebrity/CelebrityPhoto", stringContent2);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    message = response.Content.ReadAsAsync<string>().Result;
+                    if (message == "Success")
+                    {
+                        ViewBag.Message = "Upload Success";
+                        return RedirectToAction("Changesetting", new { id = model.CelebrityId });
+                    }
+                    else
+                    {
+                        ViewBag.Message = "Upload Failed";
+                        return RedirectToAction("ViewImage", new { id = model.CelebrityId });
+                    }
+                }
+                else
+                {
+                    ViewBag.Message = "Upload Failed";
+                    return RedirectToAction("ViewImage", new { id = model.CelebrityId });
+                }
+                //
+            }
+            ViewBag.Message = "Upload Failed";
+            return RedirectToAction("ViewImage", new { id = model.CelebrityId });
+        }
+        
+
     }
 }
